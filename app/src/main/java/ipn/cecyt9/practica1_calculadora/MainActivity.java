6a07c10311b22e54d46ac6022e694b981540597f
SaiaCohen <legendcreeper2345@gmail.com>
2021-09-01 17:05:46 -0500

    Initial commit

---
+++ app/src/main/java/ipn/cecyt9/practica1_calculadora/MainActivity.java
@@ -0,0 +1,137 @@
+package ipn.cecyt9.practica1_calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    Double numero1,numero2,resultado;
+    String operador;
+
+    //alt shift enter
+    public void onClickButtonOne(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "1");
+    }
+
+    public void onClickButtonTwo(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "2");
+    }
+
+    public void onClickButtonThree(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "3");
+    }
+
+    public void onClickButtonFour(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "4");
+    }
+
+    public void onClickButtonFive(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "5");
+    }
+
+    public void onClickButtonSix(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "6");
+    }
+
+    public void onClickButtonSeven(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "7");
+    }
+
+    public void onClickButtonEight(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "8");
+    }
+
+    public void onClickButtonNine(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "9");
+    }
+
+    public void onClickButtonZero(View miView){
+        TextView tv = findViewById(R.id.textView);
+        String value = tv.getText().toString();
+        tv.setText(value + "0");
+    }
+
+    public void onClickOperacionCapturaNumero1(View miView){
+        TextView tv = (TextView) findViewById(R.id.textView);
+        numero1 = Double.parseDouble(tv.getText().toString());
+        tv.setText("");
+    }
+
+    public void onClickSuma(View miView){
+        operador="+";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickResta(View miView){
+        operador="-";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickMulti(View miView){
+        operador="*";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickDiv(View miView){
+        operador="/";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickIgual(View miView){
+        TextView tv = (TextView) findViewById(R.id.textView);
+        numero2 = Double.parseDouble(tv.getText().toString());
+
+        try{
+            if (operador.equals("+")){
+                resultado = numero1 + numero2;
+            }
+
+            if (operador.equals("-")){
+                resultado = numero1 - numero2;
+            }
+
+            if (operador.equals("*")){
+                resultado = numero1 * numero2;
+            }
+
+            if (operador.equals("/")){
+                resultado = numero1 / numero2;
+            }
+            tv.setText(resultado.toString());
+        }catch(NumberFormatException nfe){
+            Toast.makeText(this, "Numero Incorrecto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+}
